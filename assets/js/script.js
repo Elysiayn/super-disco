c6af64a92c892c8718d66e35a19edf5352615b8e
Elysiayn <wendy.lemus.wl@gmail.com>
2020-11-13 01:25:19 -0500

    changed time format

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,5 @@
 // using moment.js to display current date in a div
 var NowMoment = moment();
 var eDisplayMoment = document.getElementById('currentDay');
-eDisplayMoment.innerHTML = NowMoment.format('[It is ]dddd, MMMM Do, [at] h:mm A');
+eDisplayMoment.innerHTML = NowMoment.format('[The date is ]dddd, MMMM Do YYYY. [The time is ] h:mm A');
 
