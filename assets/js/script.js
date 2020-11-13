3f33f84bb7d1b7431c4495da36a4a6c7941120a5
Elysiayn <wendy.lemus.wl@gmail.com>
2020-11-13 01:21:34 -0500

    added hour slots and save button visual

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,6 @@
-//display current date in header
-//const today = moment ();
-//console.log(
-//    "Today's date is " +
-//    today.format('DD-MM-YYYY')
-//);
 
-//var d = new Date();
-//document.getElementById("currentDay").innerHTML = d;
-
-// useing moment.js to display current date in a div
+// using moment.js to display current date in a div
 var NowMoment = moment();
 var eDisplayMoment = document.getElementById('currentDay');
-eDisplayMoment.innerHTML = NowMoment.format('[Todays Date is ] dddd, MMMM Do');
+eDisplayMoment.innerHTML = NowMoment.format('[It is ]dddd, MMMM Do, [at] h:mm A');
+
