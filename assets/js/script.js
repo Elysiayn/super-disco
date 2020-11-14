cabf2ce9025a3e5de8f3771fa6411ea1bd23551a
Elysiayn <wendy.lemus.wl@gmail.com>
2020-11-13 22:25:05 -0500

    added js function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,8 @@ var NowMoment = moment();
 var eDisplayMoment = document.getElementById('currentDay');
 eDisplayMoment.innerHTML = NowMoment.format('[The date is ]dddd, MMMM Do YYYY. [The time is ] h:mm A');
 
+var tasks = {};
+
+var createTask = function (taskText, taskDate) {
+    
+}
