5f4d48a6e6fd0b9027dbc918dcbc960c2173b0a3
Elysiayn <wendy.lemus.wl@gmail.com>
2020-11-15 20:47:57 -0500

    found bug in time function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,64 @@ var NowMoment = moment();
 var eDisplayMoment = document.getElementById('currentDay');
 eDisplayMoment.innerHTML = NowMoment.format('[The date is ]dddd, MMMM Do YYYY. [The time is ] h:mm A[.]');
 
-//save vale (input info in text area) to local storage on click of saveBtn
+//save value (input info in text area) to local storage on click of saveBtn
 $(".saveBtn").on("click", function (event) {
-    var saveValue = $(this).siblings("textarea").val().trim();
-    var saveTime = $
-    localStorage.setItem(saveValue);
+    var saveDescription = $(this).siblings("textarea").val().trim();
+ // save id of row w/ text input
+    var saveDescriptionID = $(this).siblings("textarea").attr("id");
+    localStorage.setItem(saveDescription, saveDescritionID);
+    console.log(localStorage);
 });
+ 
 
-// write function that displays task on page
-//need to know where to put task on page (maybe array w/ id to save into 
-//local storage along w/ value)
-//when set value into local storage can also set time into local storage, 
-//when saveBtn clicked write another variable that can save time as well
+// function to load events
+var loadEvents = function() {
+    $("#0").val(localStorage.getItem("0"))
+    $("#1").val(localStorage.getItem("1"))
+    $("#2").val(localStorage.getItem("2"))
+    $("#3").val(localStorage.getItem("3"))
+    $("#4").val(localStorage.getItem("4"))
+    $("#5").val(localStorage.getItem("5"))
+    $("#6").val(localStorage.getItem("6"))
+    $("#7").val(localStorage.getItem("7"))
+    $("#8").val(localStorage.getItem("8"))
+    $("#9").val(localStorage.getItem("9"))
+    $("#10").val(localStorage.getItem("10"))
+}
+
+// Check time function and apply corresponding class
+var checkTimes = function() {
+    // select all description block elements
+    var descriptionBlockElements = $(".description");
+
+    // loop through description classes
+    for (var i = 0; i < descriptionBlockElements.length; i++) {
+
+        // get description elements by ID
+        var manipID = document.getElementById(descriptionBlockElements[i].id)
+
+        // Get description arrays ID's as a string
+        var descriptionID = descriptionBlockElements[i].id;        
+
+        // remove any old classes from the element
+        $(descriptionBlockElements[i].id).removeClass(".present .past .future");
+
+        // apply new class if task is present/past/future
+        if (descriptionID < now) {
+            $(manipID).addClass("past");
+        } else if (descriptionID > now) {
+            $(manipID).addClass("future");
+        } else {
+            $(manipID).addClass("present");
+        }
+    }
+
+}
+
+// run checkTime every 5 minutes
+setInterval(checkTimes(), (1000 * 60) * 5);
+
+loadEvents()
+
+
+
